af7f90c98b50e71b23f65c76014bbb9f52e018fb
parkdonggun <hbd9425@gmail.com>
2023-10-31 11:37:14 +0900

    라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./page/Home";
 
 function App() {
   return (
     <div className="App">
-      <div className=" text-red-500">test</div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
