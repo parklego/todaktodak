df7a9d451dcd50b4c8ea854490c94a15a712f62e
parkdonggun <hbd9425@gmail.com>
2023-10-31 14:56:11 +0900

    홈 화면에서 로그인 유무 분기

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,31 +1,16 @@
 import React from "react";
-import Input from "../component/Input";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, db } from "../firebase";
+import Login from "../component/Login";
 
 const Home = () => {
-  return (
-    <div className="flex flex-wrap content-center lg:items-center h-screen ">
-      <div className="w-full lg:w-5/12">
-        <div className="flex flex-col justify-center items-center  ">
-          <img className=" w-3/5" src="/asset/logo.jpg" alt="logo" />
-          <Input type="text" id="아이디"></Input>
-          <Input type="password" id="비밀번호"></Input>
-        </div>
+  const [user, loading] = useAuthState(auth);
 
-        <div className="flex justify-center ">
-          <button className="w-3/6 border-2 p-2 m-12 rounded hover:bg-yellow-200">
-            구글 로그인
-          </button>
-        </div>
-      </div>
-      <div className=" hidden lg:block w-full lg:w-7/12">
-        <img
-          className=" lg:w-screen lg:h-screen"
-          src="/asset/bg.jpg"
-          alt="background"
-        ></img>
-      </div>
-    </div>
-  );
+  if (!user) {
+    return <Login />;
+  }
+
+  return <div>로그인 성공</div>;
 };
 
 export default Home;
