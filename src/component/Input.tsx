8d87017abe61ac2aee2a68c410cf01866a6138c2
parkdonggun <hbd9425@gmail.com>
2023-10-31 11:36:52 +0900

    input컴포넌트 및 홈 화면 스타일링

---
+++ src/component/Input.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+interface InputProps {
+  type: string;
+  id: string;
+}
+
+const Input = ({ type, id }: InputProps) => {
+  return (
+    <div className={`flex flex-col my-2 w-3/6`}>
+      <label className="my-2">{id}</label>
+      <input className="h-10 border-2 rounded" type={type}></input>
+    </div>
+  );
+};
+
+export default Input;
---
+++ src/page/Home.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Input from "../component/Input";
+
+const Home = () => {
+  return (
+    <div className="flex flex-wrap items-between lg:items-center h-screen ">
+      <div className="w-full lg:w-5/12">
+        <div className="flex flex-col justify-center items-center  ">
+          <img className=" w-3/5" src="/asset/logo.jpg" alt="logo" />
+          <Input type="text" id="아이디"></Input>
+          <Input type="password" id="비밀번호"></Input>
+        </div>
+
+        <div className="flex justify-center ">
+          <button className="w-3/6 border-2 p-2 m-12 rounded hover:bg-yellow-200">
+            구글 로그인
+          </button>
+        </div>
+      </div>
+      <div className=" max- w-full lg:w-7/12">
+        <img
+          className="hidden lg:block lg:w-screen lg:h-screen"
+          src="/asset/bg.jpg"
+          alt="background"
+        ></img>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
