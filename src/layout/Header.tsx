84733a82bb7245514fc389c48f44ac828b5f205d
parkdonggun <hbd9425@gmail.com>
2023-11-01 13:35:47 +0900

    헤더 컴포넌트 추가

---
+++ src/layout/Header.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { signOut } from "firebase/auth";
+import { auth } from "../firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+
+const Header = () => {
+  const [user] = useAuthState(auth);
+
+  const logOut = () => {
+    signOut(auth);
+  };
+
+  return (
+    <div className="flex flex-row justify-between px-6 py-2 border">
+      <div className="flex flex-row w-24 items-center justify-evenly">
+        <img
+          className=" w-9 h-9 rounded-full"
+          src={user?.photoURL!}
+          alt="user profile"
+        />
+        <p>{user?.displayName}</p>
+      </div>
+
+      <button onClick={logOut}>로그아웃</button>
+    </div>
+  );
+};
+
+export default Header;
