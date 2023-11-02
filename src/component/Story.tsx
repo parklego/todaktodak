e88c831ab907fed12f83628ec8e9c3eae04791f8
parkdonggun <hbd9425@gmail.com>
2023-11-03 00:22:55 +0900

    사연 리스트 정렬 방법 수정

--- src/component/Story.tsx
+++ src/component/Story.tsx
@@ -2,13 +2,13 @@ import React from "react";
 import { Accordion } from "@chakra-ui/react";
 import Accordions from "./Accordions";
 import { db } from "../firebase"; // 위에서 정의한 firebase 설정 파일
-import { collection, query } from "firebase/firestore";
+import { collection, query, orderBy } from "firebase/firestore";
 import { useCollectionData } from "react-firebase-hooks/firestore";
 import { StoryItem } from "../types";
 const Story = () => {
   // Todo : 각 story items firebase에서 가져와서 map으로 뿌려주기
 
-  const q = query(collection(db, "story"));
+  const q = query(collection(db, "story"), orderBy("timestamp", "desc"));
 
   const [storyList] = useCollectionData(q);
 
