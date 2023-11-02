fc9acf90c8949e3d79f97822f52a4ed57bc9cbc1
parkdonggun <hbd9425@gmail.com>
2023-11-02 19:38:03 +0900

    모바일환경에서 하단단nav바로 인해 숨겨지는 이슈 해결

--- src/component/Story.tsx
+++ src/component/Story.tsx
@@ -6,7 +6,7 @@ const Story = () => {
   // Todo : 각 story items firebase에서 가져와서 map으로 뿌려주기
   return (
     <div className=" lg:flex lg:justify-center p-5">
-      <Accordion className="h-[70vh] overflow-scroll lg:w-[80%] " allowToggle>
+      <Accordion className="h-[60vh] overflow-scroll lg:w-[80%] " allowToggle>
         <Accordions title="test" content="contents" />
         <Accordions title="test" content="contents" />
       </Accordion>
