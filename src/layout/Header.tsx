10a427155eafe799b27ddfa5729dd0267733ffe7
parkdonggun <hbd9425@gmail.com>
2023-11-03 09:26:54 +0900

    새로고침 시 유저 프로필 사진 업데이트시 잠깐 엑박 뜨는 부분 해결

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -17,11 +17,13 @@ const Header = () => {
   return (
     <div className="flex flex-row justify-between px-4 py-2 border">
       <div className="flex flex-row w-32 items-center justify-evenly">
-        <img
-          className=" w-9 h-9 rounded-full"
-          src={user?.photoURL!}
-          alt="user profile"
-        />
+        {user?.photoURL && (
+          <img
+            className=" w-9 h-9 rounded-full"
+            src={user.photoURL}
+            alt="profile"
+          />
+        )}
 
         <p>{user?.displayName}</p>
       </div>
